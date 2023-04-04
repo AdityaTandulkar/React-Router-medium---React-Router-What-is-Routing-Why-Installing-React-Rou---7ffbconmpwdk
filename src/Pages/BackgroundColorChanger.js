4a43dccb3175abd7180d0b1cf53da52a8aeb4c7d
aditya <adityatandulkar20@gmail.com>
2023-04-04 16:15:37 +0530

    Initial commit

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 
 export const BackgroundColorChanger = () =>{
+    let {colorname} = useParams();
+    console.log(colorname);
+    
     return(
-       
+       <main style={{ height: "100vh", backgroundColor: `${colorname}` }}>
+       Background Color Change
+       </main>
     )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { AppRoutes } from '../AppRoutes';
 
 import '../styles/App.css';
@@ -7,6 +7,8 @@ const App = () => {
 
   return (
     <BrowserRouter>
+      <Link to='/color/blue'>Color</Link>
+      <Link to={`/calculator?num1=&num2=&op=`}>Calculator</Link>
       <AppRoutes />
     </BrowserRouter>
 
